0d104decdefa5d4f705e0e0a78f25b6d23e152b9
Chars <l22390451@chetumal.tecnm.mx>
2024-09-23 23:49:40 -0500

    Separacion de la conexion de la base de datos del CRUD de los productos

--- crud_productos.php
+++ crud_productos.php
@@ -1,16 +1,6 @@
 <?php
-// Conexión a la base de datos
-$host = 'localhost';
-$dbname = 'Steren';
-$username = 'root';
-$password = '';
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error al conectar a la base de datos: " . $e->getMessage());
-}
+// Incluir la conexión a la base de datos
+require_once 'db.php';
 
 // Definir la acción (crear, leer, actualizar, eliminar)
 $action = isset($_GET['action']) ? $_GET['action'] : 'read';
